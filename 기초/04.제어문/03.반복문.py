20a9b17c7757fa6ed15550faa9ba0e1f26bcecff
BuildEnough <rhtmaehcl878@naver.com>
2025-03-09 17:08:29 +0900

    파이썬 내공쌓기(반복문)

--- 기초/04.제어문/03.반복문.py
+++ 기초/04.제어문/03.반복문.py
@@ -1,3 +1,4 @@
+""" for 반복문
 for i in range(3):
     print("말티즈")
 
@@ -6,4 +7,10 @@ for i in range(0, 3):
     print("과제 완료")
 
 for i in range(1, 10, 2):
+    print(i)"
+"""
+
+i = 0
+while i < 3:
     print(i)
+    i += 1
